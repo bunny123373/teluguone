c4ca04b180b841b46363fd3921170f284598c6f4
Balu <balu@gmail.com>
2026-03-14 19:22:58 +0530

    Remove loading spinner from player page

--- src/app/player/[id]/PlayerPageClient.tsx
+++ src/app/player/[id]/PlayerPageClient.tsx
@@ -43,9 +43,7 @@ export default function PlayerPageClient({ id }: PlayerPageClientProps) {
 
   if (loading) {
     return (
-      <main className="min-h-screen bg-black flex items-center justify-center">
-        <div className="w-8 h-8 border-2 border-white/20 border-t-white rounded-full animate-spin"></div>
-      </main>
+      <main className="min-h-screen bg-black"></main>
     );
   }
 
